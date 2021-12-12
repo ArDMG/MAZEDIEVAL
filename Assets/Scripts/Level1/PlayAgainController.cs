4f93be1d4603216fcf7f5acf5ad64a2fb33b2ce1
ArDMG <arielsergiodimango@gmail.com>
2021-12-12 14:46:14 -0300

    [UPDATE] FULL MAZE

--- Assets/Scripts/Level1/PlayAgainController.cs
+++ Assets/Scripts/Level1/PlayAgainController.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class PlayAgainController : MonoBehaviour
 {
-    
+    [System.Obsolete]
     public void OnClickReplay()
     {
         Debug.Log("Start!");
