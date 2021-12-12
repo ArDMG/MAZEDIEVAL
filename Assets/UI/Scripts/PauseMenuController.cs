c09df8b8b29c3a2e60a726bde02a5f6f3886835e
ArDMG <arielsergiodimango@gmail.com>
2021-12-11 21:48:49 -0300

    [ADD] Pause Menu

--- Assets/UI/Scripts/PauseMenuController.cs
+++ Assets/UI/Scripts/PauseMenuController.cs
@@ -4,21 +4,45 @@ using UnityEngine;
 
 public class PauseMenuController : MonoBehaviour
 {
-
+    private bool isActiveMENU = false;
     public GameObject PauseMenu;
     // Start is called before the first frame update
     void Start()
     {
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+ 
+        if (Input.GetKeyDown(KeyCode.Escape) && isActiveMENU == false)
         {
-            Debug.Log("Pause");
+            Debug.Log("ActivatePause");
             PauseMenu.SetActive(true);
+            isActiveMENU = true;
+            Cursor.lockState = CursorLockMode.None;
 
         }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Escape) && isActiveMENU == true)
+            { 
+              Debug.Log("DeactivatePause");
+            PauseMenu.SetActive(false);
+            isActiveMENU = false;
+            Cursor.lockState = CursorLockMode.Confined;
+            }
+        }
+        }
     }
-}
+    //private void DeactivePause()
+    //{
+       // if (isActiveMENU == true && Input.GetKeyDown(KeyCode.Escape))
+      //  {
+   //         Debug.Log("DeactivatePause");
+     //       PauseMenu.SetActive(true);
+      //      isActiveMENU = false;
+//
+//
+    //    }
+   // }
+//}
